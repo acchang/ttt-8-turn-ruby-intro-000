a0d67a01c0e6fa5074e8ad0a44af24f2208102c8
Andrew C <acchang4@gmail.com>
2020-04-09 23:37:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,10 +18,10 @@ def position_taken?(board, index)
   end
 end
 
-  def valid_move?(board, index)
-      if !position_taken?(board, index) && index.between?(0, 8)
-          return true
-        else
-          return false
+def valid_move?(board, index)
+    if !position_taken?(board, index) && index.between?(0, 8)
+        return true
+              else
+        return false
         end
       end
