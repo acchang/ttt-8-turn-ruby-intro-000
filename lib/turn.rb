b5e25525b691fbc6b5f49f80a824ed8b2864902d
Andrew C <acchang4@gmail.com>
2020-04-10 00:00:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,11 +35,6 @@ def turn(board)
   input=gets.strip
   index = input_to_index(input)
   valid_move?(board, index)
-  until valid_move?(board, index) = true
-    puts "Please enter 1-9:"
-    input=gets.strip
-    index = input_to_index(input)
-
-    move(board, index, player="X")
+  move(board, index, player="X")
 end
 end
